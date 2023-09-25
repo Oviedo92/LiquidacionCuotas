a439134caebbf4d4988695655ce625b4d20c69b4
Oviedo92 <eleonardooviedo@unicesar.edu.co>
2023-09-25 13:04:54 -0500

    CapasLiquidacionCuotas

---
+++ Logica/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+using Entidad;
+
+namespace Logica
+{
+    public class Class1
+    {
+    }
+}
